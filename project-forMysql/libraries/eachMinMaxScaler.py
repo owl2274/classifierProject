19ce5c4978bf490e72259029aff2a1c391a55dda
jae2274 <owl2274@gmail.com>
2017-12-22 17:11:34 +0900

    code refactoring:eachMinMaxScaler.py is more simple

--- project-forMysql/libraries/eachMinMaxScaler.py
+++ project-forMysql/libraries/eachMinMaxScaler.py
@@ -3,16 +3,9 @@ from sklearn.preprocessing import MinMaxScaler
 
 class EachMinMaxScaler():
     def __init__(self,scaleList):
-
         self.scalerList = scaleList
+
     def fit_transform(self,dataSet):
-        transformedDataSet=np.copy(dataSet)
-
-
-        for i,scaleSize in enumerate(self.scalerList):
-
-            if scaleSize == 0:
-                transformedDataSet[:,i] = np.zeros_like(transformedDataSet[:,i])
-            else:
-                transformedDataSet[:,i] = MinMaxScaler(feature_range=(0,scaleSize)).fit_transform(transformedDataSet[:,i].reshape(-1,1)).reshape(1,-1)
+        transformedDataSet = MinMaxScaler().fit_transform(np.copy(dataSet))
+        transformedDataSet = transformedDataSet * self.scalerList
         return transformedDataSet
